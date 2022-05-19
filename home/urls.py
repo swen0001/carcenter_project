05c8f66ec44974541e54ec86d88a6b0814cd5b62
swen0001 <oleksandr.venger0001@gmail.com>
2022-05-19 23:21:26 +0300

    add navigation, and page: contact, news, about

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from . import views
 
 
 urlpatterns = [
-    path('', index),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
+    path('cars', views.cars, name='cars'),
+    path('news', views.news, name='news'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'home/index.html')
+def home(request):
+    return render(request, 'home/home.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def cars(request):
+    return render(request, 'home/about.html')
+
+
+def contacts(request):
+    return render(request, 'home/contacts.html')
+
+
+def news(request):
+    return render(request, 'home/news.html')
