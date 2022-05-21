262a8ab1dd130a25fe1b6611fc958f4a54ce90d6
swen0001 <oleksandr.venger0001@gmail.com>
2022-05-21 11:46:32 +0300

    Team admin customization

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+
+    def avatar(self, object):
+        return format_html('<img src="{}" width="60" style="border-radius: 50px" />'.format(object.photo.url))
+
+    list_display = ('id', 'avatar', 'first_name', 'last_name', 'designation', 'created_date')
+    list_display_links = ('id', 'avatar', 'first_name', 'last_name', 'designation')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('designation',)
+
+
+admin.site.register(Team, TeamAdmin)
