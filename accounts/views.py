501436f19e76dc9a1dbe231d2ae41dfea55e4bbd
swen0001 <oleksandr.venger0001@gmail.com>
2022-05-27 14:36:23 +0300

    register page update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html ')
+
+
+def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Нікнейм вже зайнято :(')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email вже зайнято :(')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname,
+                                                    last_name=lastname,
+                                                    username=username,
+                                                    email=email,
+                                                    password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'Ваш аккаунт авторизовано :)')
+                    return redirect('dashboard')
+                    user.save()
+                    messages.success(request, 'Ви успішно зареєстровані')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Паролі не співпадають :(')
+            return redirect('register')
+    return render(request, 'accounts/register.html ')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html ')
+
+
+def logout(request):
+    return redirect('home')
+
+
+
--- carcenter/urls.py
+++ carcenter/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('cars/', include('cars.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.index_title = 'CarCenter administration'
