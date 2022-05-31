713b7bc95841d6c0949004b7b061b4defae329fb
swen0001 <oleksandr.venger0001@gmail.com>
2022-05-31 16:19:20 +0300

    started deployment

--- cars/views.py
+++ cars/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 def cars(request):
 
     cars = Car.objects.order_by('-created_date')
-    paginator = Paginator(cars, 3)
+    paginator = Paginator(cars, 4)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
 
--- home/views.py
+++ home/views.py
@@ -4,12 +4,16 @@ from accounts.models import MyUser
 from .models import Team
 from cars.models import Car
 from django.core.mail import send_mail
+from django.core.paginator import Paginator
 
 
 def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(all_cars, 3)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
 
     brand_search = Car.objects.values_list('brand', flat=True).distinct()
     region_search = Car.objects.values_list('region', flat=True).distinct()
@@ -18,7 +22,7 @@ def home(request):
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
-        'all_cars': all_cars,
+        'all_cars': paged_cars,
         'brand_search': brand_search,
         'region_search': region_search,
         'year_search': year_search,
